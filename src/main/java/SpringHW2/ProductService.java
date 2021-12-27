c695e40a6d56d9fe0707196fe480b2cca1e5d572
Stepan <eronin2002@gmail.com>
2021-12-27 18:16:14 +0200

    SPRHomeWork2

---
+++ src/main/java/SpringHW2/ProductService.java
@@ -0,0 +1,18 @@
+package SpringHW2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductService {
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public String getTitleById(Long id) {
+        return productRepository.findProductById(id).getTitle();
+    }
+}
